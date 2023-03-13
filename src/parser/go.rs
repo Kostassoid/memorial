195191749e199cacb237f9d5c872fee2922f8ef1
kostassoid <k.alexandroff@gmail.com>
2023-03-13 23:47:53 +0100

    Fix indentation issues

--- src/parser/go.rs
+++ src/parser/go.rs
@@ -1,11 +1,31 @@
 use pest::Parser as P;
 use pest_derive::Parser;
 use anyhow::Result;
+use pest::iterators::Pair;
 use crate::parser::{Quote, FileParser};
 
 #[derive(Parser)]
 #[grammar = "src/parser/go.pest"]
 pub struct GoParser;
+
+impl GoParser {
+    //todo: extract or make part of the pest rules
+    fn rule_to_quote(pair: Pair<Rule>) -> Quote {
+        let line = pair.line_col().0;
+        let indent = pair.line_col().1 - 1;
+
+        let body = pair.into_inner().as_str()
+            .replace(&format!("\n{}", " ".repeat(indent)), "\n")
+            .replace(&format!("\n{}", "\t".repeat(indent)), "\n")
+            .trim()
+            .to_string();
+
+        Quote {
+            line,
+            body,
+        }
+    }
+}
 
 impl FileParser for GoParser {
     fn parse_from_str(&self, source: &str) -> Result<Vec<Quote>> {
@@ -13,12 +33,7 @@ impl FileParser for GoParser {
         Ok(parsed
             .filter_map(|p| {
                 match p.as_rule() {
-                    Rule::COMMENT => Some(
-                        Quote {
-                            line: p.line_col().0,
-                            body: p.into_inner().as_str().trim().to_string(),
-                        }
-                    ),
+                    Rule::COMMENT => Some(GoParser::rule_to_quote(p)),
                     _ => None
                 }
             })
@@ -47,7 +62,7 @@ mod tests {
        "#).unwrap();
 
         let expected = vec!(
-            Quote { body: "Block comment\n        is long".to_string(), line: 4 },
+            Quote { body: "Block comment\nis long".to_string(), line: 4 },
             Quote { body: "Inline comment".to_string(), line: 10 },
             Quote { body: "Another inline comment".to_string(), line: 11 }
         );
